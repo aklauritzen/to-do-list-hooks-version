07c2e67d1b129e4394a182f40391182e7f3ef1da
redhawkdk <aklauritzen@gmail.com>
2021-01-31 16:27:16 +0100

    CSS - Layout, fonts, title, icons, style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 // TODO: Connect app to database (MongoDB?)
-// TODO: Undo complete
+// TODO: #8 Completed tasks background should be greyed out
+// TODO: #5 Undo complete
+// TODO: #6 If modified, then change meta to modified date: Modified 23-01-2021 23:00
+// TODO: #7 Add optional task desription, and change current todo.text to taskTitle
+// TODO: #10 Completed task should move to the end of the to do list. animated move?
 
 import React from 'react';
 import './App.css';
@@ -11,10 +15,17 @@ function Todo({ todo, index, completeTodo, removeTodo }) {
       className="todo"
       style={{ textDecoration: todo.isCompleted ? "line-through" : ""}}
     >
-      {todo.text} - created: {todo.currentDateFormated}
+      <div className="title-meta-container">
+        <div className="todo-title">
+          {todo.text}
+        </div>
+        <div className="todo-meta">
+          created: {todo.currentDateFormated}
+        </div>
+        </div>
       <div>
-        <button onClick={() => completeTodo(index)}>Complete</button>
-        <button onClick={() => removeTodo(index)}>x</button>
+        <button onClick={() => completeTodo(index)}><img className="todo-icon" alt="Checkmark icon" src="complete-todo-icon.svg" /></button>
+        <button onClick={() => removeTodo(index)}><img className="todo-icon" alt="Delete icon" src="delete-todo-icon.svg" /></button>
       </div>
     </div>
   );
@@ -37,9 +48,10 @@ function TodoForm({ addTodo }) {
   };
 
   // On input submit call
-  return (
-    <form onSubmit={handleSubmit}>
+  return (    
+    <form className="title-form" onSubmit={handleSubmit}>
       <input
+        placeholder="Enter Task title" 
         type="text"
         className="input"
         value={value}
@@ -51,7 +63,7 @@ function TodoForm({ addTodo }) {
 
 function App() {
   
-  // Todo object list
+  // Todo object list with test data
   const [todos, setTodos] = React.useState([   
     { text: "First task", 
       isCompleted: false,
@@ -69,12 +81,18 @@ function App() {
     },
   ]);
 
-  // Used if list should start empty
+  // Used in production if list should be empty at the beginning
   // const [todos, setTodos] = React.useState([]);
 
   // Current date and time for Todo
-  const currentDate = new Date();
-  const currentDateFormated = currentDate.getDate() + "-" + currentDate.getMonth() + 1 + "-" + currentDate.getFullYear() + " " + currentDate.getHours() + ":" + currentDate.getMinutes();
+  const today = new Date();
+  const date = ('0' + parseInt(today.getDate())).slice(-2);
+  const month = ('0' + parseInt(today.getMonth() + 1)).slice(-2);
+  const year = today.getFullYear();
+  const hours = ('0' + parseInt(today.getHours() + 1)).slice(-2);
+  const minutes = today.getMinutes();
+   
+  const currentDateFormated = date + "-" + month + "-" + year + " " + hours + ":" + minutes;
 
   // Adds a new todo
   const addTodo = text => {
@@ -102,6 +120,10 @@ function App() {
   // Mapping over the todo items and display them by index
   return (
     <div className="app">
+      <h1>To Do List</h1>
+      <em>React Hook Version</em>
+      <h3>Create task</h3>
+      <TodoForm addTodo={addTodo} />
       <div className="todo-list">        
         {todos.map((todo, index) => (
           <Todo
@@ -113,7 +135,7 @@ function App() {
           />
         ))}
 
-        <TodoForm addTodo={addTodo} />
+        
       </div>
     </div>
   );
